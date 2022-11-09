e2f7dd3cca9da9e9b7ccac8bc7b3e0a6e1bb64fd
Javier Agudo <jagudox@gmail.com>
2022-11-09 18:56:44 +0100

    Formulario finished, it create new post and has some validators.

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Post } from 'src/app/interfaces/post.interface';
 import { PostService } from 'src/app/services/post.service';
 @Component({
   selector: 'app-formulario',
@@ -9,19 +10,30 @@ import { PostService } from 'src/app/services/post.service';
 export class FormularioComponent implements OnInit {
   formulario: FormGroup
   arrCategories: string[]
+  regularExp: RegExp
   constructor(private postsService: PostService) {
+    this.regularExp = new RegExp('^http(s)?\://[a-zA-Z0-9\-\.]+\.')
     this.arrCategories = this.postsService.getAllCategories()
     this.formulario = new FormGroup({
-      title: new FormControl(),
-      text: new FormControl(),
-      author: new FormControl(),
-      image: new FormControl(),
-      date: new FormControl(),
-      category: new FormControl(),
+      title: new FormControl('', [Validators.required]),
+      text: new FormControl('', [Validators.required]),
+      author: new FormControl('', [Validators.required]),
+      image: new FormControl('', [Validators.required, Validators.pattern(this.regularExp)]),
+      date: new FormControl('', [Validators.required]),
+      category: new FormControl('', [Validators.required]),
     })
   }
 
   ngOnInit(): void {
   }
 
+  onSubmit(formu: any) {
+    let newPost = formu.value
+    formu.reset()
+    return this.postsService.createPost(newPost)
+  }
+
+  errorChecker(field: string, error: string): boolean | undefined {
+    return this.formulario.get(field)?.hasError(error) && this.formulario.get(field)?.touched
+  }
 }
